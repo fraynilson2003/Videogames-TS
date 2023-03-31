75609d18af187b96ba245559245696c963c4504f
fraynilson2003 <fraynilson2003@gmail.com>
2023-03-30 22:01:19 -0500

    ss

--- client/src/components/Hero/Filter/Filter.jsx
+++ client/src/components/Hero/Filter/Filter.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export default function Filter() {
+  let genders = useSelector(state => state.allGenders)
+
   let tukis = [
-    {filter: "name", option: ["Accion", "Aventura", "Comedia", "Hot", "Nice"]},
+    {filter: "name", option: [...genders]},
     {filter: "rating", option: ["Accion", "Aventura", "Comedia", "Hot", "Nice"]},
     {filter: "gender", option: ["Accion", "Aventura", "Comedia", "Hot", "Nice"]},
     {filter: "order", option: ["Accion", "Aventura", "Comedia", "Hot", "Nice"]},
-    {filter: "gender", option: ["Accion", "Aventura", "Comedia", "Hot", "Nice"]},
-    {filter: "gender", option: ["Accion", "Aventura", "Comedia", "Hot", "Nice"]},
   ]
 
 
@@ -18,14 +19,14 @@ export default function Filter() {
       <div className='flex flex-row overflow-x-scroll filter justify-start items-center' >
         <input placeholder='search name' type="text" className='bg-gris text-blanco rounded px-2 mx-2 h-[30px] hover:text-oscuro hover:bg-blanco' />
 
-        {tukis?.length && tukis.map((filt, ind)=>
+        {genders?.length && tukis.map((filt, ind)=>
           <select 
             className="select-bordered 
             w-auto whitespace-nowrap px-2 mx-2 text-sm bg-gris h-[30px] rounded"
           >
             <option disabled selected>Who shot first?</option>
             {filt.option.map((e, i)=>
-              <option key={i}>{e}</option>
+              <option value={e.id} key={i}>{e.name}</option>
             )}
           </select>
         )}
--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -9,36 +9,17 @@ export const getAllVideogames = ()=>async(dispatch)=>{
   }
 }
 
-/********** POST ******************** */
-export const postRegisterVideogame = (statement)=>async(dispatch)=>{
-  console.log("LLEGa");
+export const getAllGenders = ()=>async(dispatch)=>{
   try {
-    let newVidGame = {
-      newVideoGame: {
-        name: statement.name,
-        description: statement.description,
-        rating: statement.rating,
-        released: statement.released,
-        background_image: statement.background_image
-        },
-      idGener: statement.idGener,
-      idPlatform: statement.idPlatform
-    }
-    console.log(/*/////////*/);
-    console.log(newVidGame);
+    let result = await axios.get(`/gender`)
 
-    let result = await axios.post("/videogames", newVidGame)
+    console.log(result.data.genders);
 
-    if(result.status == 200){
-      console.log(result.status);
-      return dispatch ({type:SET_VIDEOGAME_RESULT,payload: true})
-      
-    }
-    console.log(result.status);
+    return dispatch ({type:CONSTANTS.GET_ALL_GENERS, payload: result.data.genders})
   } catch (error) {
-    console.log("ERROR "+error);
-    return dispatch ({type:SET_VIDEOGAME_RESULT,payload: false})
-
+    let msg = (error.message? error.message : "Error en action getAllGenders")
+    alert(msg)
+    throw new Error(msg)
   }
-
 }
+
