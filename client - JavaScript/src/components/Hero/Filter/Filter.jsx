f7e451d0dba9f6a2548d0e3130e0c2e8854af2b0
fraynilson2003 <fraynilson2003@gmail.com>
2023-05-05 19:09:52 -0500

    assusus

--- client - JavaScript/src/components/Hero/Filter/Filter.jsx
+++ client - JavaScript/src/components/Hero/Filter/Filter.jsx
@@ -78,7 +78,7 @@ export default function Filter({isLoadingVideogame, setIsLoadingVideogame}) {
     <div className='flex flex-row mt-2 object-cover bg-fondo w-full h-[60px]  p-3  px-3 shadow-custom 
       md:flex-col md:w-[240px] md:p-0 md:m-3 md:mr-2 md:shadow-none md:rounded-md md:mt-3 md:h-[300px]   xl:rounded xl:mx-auto'>
 
-    <NavLink to={"/create"} className='hidden md:w-full md:mt-4 md:mb-4'>
+    <NavLink to={"/create"} className='hidden md:w-full md:mt-4 md:mb-4 md:block'>
       <div className='bg-verde/60 text-blanco border-2 w-[170px] py-2 px-3 mx-auto rounded-lg cursor-pointer hover:bg-verde'>
         <p className=' text-center text-lg font-bold'>New videogame</p>
       </div>
